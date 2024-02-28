3a6bad2109b0e6c6bf26f91086f6ee4d42fdcd5d
Shawon <chiefmahedi@gmail.com>
2024-02-28 23:43:11 +0600

    Update log method in Logger class to use the LogObject as parameter

--- src/Logger/Logger.ts
+++ src/Logger/Logger.ts
@@ -1,13 +1,13 @@
 import * as fs from 'fs';
-
+import LogObject from './LogObject';
 export default class Logger {
     private logStream: fs.WriteStream;
 
     constructor(logFilePath: string) {
         this.logStream = fs.createWriteStream(logFilePath, { flags: 'a' });
     }
-    log(message: string, ipAddress: string): void {
-        const logEntry = `${new Date().toISOString()} - ${message} - from ${ipAddress}\n`;
+    log(logObject: LogObject): void {
+        const logEntry = `${new Date().toISOString()} - ${logObject.message} - from ${logObject.ipAddress}\n`;
         this.logStream.write(logEntry);
     }
 }
