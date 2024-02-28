4b762033b01bae4bb6f6ea19a52ee6ce15935d22
Shawon <chiefmahedi@gmail.com>
2024-02-28 23:45:57 +0600

    Create a logObject with request-specific information and log the request details

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import Logger from './Logger/Logger';
-
+import LogObject from './Logger/LogObject';
 const app = express();
 const logger = new Logger('access.log');
-
+const logObject = new LogObject();
 app.get('/test',(req, res) => {
     const ipAddress = req.ip;
-    logger.log(`${req.method} ${req.url}`, ipAddress);
+    logObject.ipAddress = ipAddress;
+    logObject.message = `${req.method} ${req.url}`;
+    logger.log(logObject);
     res.send('Logger running');
 });
 
