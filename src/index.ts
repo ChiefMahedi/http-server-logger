7d9b0d467377514cf4af0ee254f5c0ec3ccbf350
Shawon <chiefmahedi@gmail.com>
2024-03-04 00:03:21 +0600

    Integrate multiple log levels in route handler

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 import express from 'express';
-import Logger from './Logger/Logger';
+import Logger, { LogLevel } from './Logger/Logger'; // Assuming Logger class and LogLevel enum are exported from './Logger'
 import LogObject from './Logger/LogObject';
+
 const app = express();
-const logger = new Logger('access.log');
-const logObject = new LogObject();
-app.get('/test',(req, res) => {
+const logger = new Logger({ logLevel: LogLevel.INFO, logFilePath: 'access.log' }); // Create Logger instance with log level and file path
+
+app.get('/test', (req, res) => {
     const ipAddress = req.ip;
+    const logObject = new LogObject();
     logObject.ipAddress = ipAddress;
     logObject.message = `${req.method} ${req.url}`;
-    logger.log(logObject);
+    logger.debug(`${req.method} ${req.url} - from ${ipAddress}`);
+    logger.info(`${req.method} ${req.url} - from ${ipAddress}`);
     res.send('Logger running');
 });
 
@@ -16,4 +19,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-
